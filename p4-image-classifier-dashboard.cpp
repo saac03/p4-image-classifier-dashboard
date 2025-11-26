c7ad5a11b3c58d56c408dbb9b8e874c94da44906
saac03 <isaac.dicayanan@gmail.com>
2025-11-27 05:28:24 +0800

    Add project files.

---
+++ p4-image-classifier-dashboard.cpp
@@ -0,0 +1,12 @@
+﻿// p4-image-classifier-dashboard.cpp : Defines the entry point for the application.
+//
+
+#include "p4-image-classifier-dashboard.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ p4-image-classifier-dashboard.h
@@ -0,0 +1,8 @@
+﻿// p4-image-classifier-dashboard.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
