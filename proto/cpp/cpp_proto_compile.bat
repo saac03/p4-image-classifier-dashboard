7f00b13866eecbbeff9b144b4bacd9b79696aae6
saac03 <isaac.dicayanan@gmail.com>
2025-11-27 07:52:45 +0800

    proto for p4

--- proto/cpp/cpp_proto_compile.bat
+++ proto/cpp/cpp_proto_compile.bat
@@ -3,7 +3,7 @@ set protobuf_cmd=%VCPKG_ROOT%\packages\protobuf_x64-windows\tools\protobuf\proto
 set grpc_exe=%VCPKG_ROOT%\installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe
 
 set src=C:\Users\Isaac\Desktop\P4\p4-image-classifier-dashboard\proto\cpp
-set proto_file=%src%\hello.proto
+set proto_file=%src%\dashboard.proto
 set dest=%src%
 
 "%protobuf_cmd%" --proto_path="%src%" --cpp_out="%dest%" --grpc_out="%dest%" --plugin=protoc-gen-grpc="%grpc_exe%" "%proto_file%" 
